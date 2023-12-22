d4f34ccaaca401e3d53f07588428d7d4cdbd8a7f
Simon De Smul <simon.desmul@alinso.group>
2023-12-22 17:17:43 +0100

    Add more code

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
 from distutils.core import setup
 setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  name = 'pyntload',         
+  packages = ['pyntload'],   
+  version = '0.0.1',      
+  license='MIT',        
+  description = 'pyntload - a simple package for importing functions from notebooks in jupyter notebook, azure databricks, ...',   # Give a short description about your library
+  author = 'Simon De Smul',                  
+  author_email = 'simon.de.smul@hotmail.com',      #
+  url = 'https://github.com/alinso-sdsmul/pyntload',   
+  download_url = 'https://github.com/alinso-sdsmul/pyntload/archive/refs/tags/0.0.1.tar.gz',  
+  keywords = ['Databricks', 'Notebooks', 'Import'],  
+  install_requires=[            
+          'requests'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',     
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
